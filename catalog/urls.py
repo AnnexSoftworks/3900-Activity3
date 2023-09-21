98b19f9b41f170071db7e8f704c4a3a838653fdb
Annex Softworks <78704624+AnnexSoftworks@users.noreply.github.com>
2023-09-21 13:10:14 -0500

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.template.defaulttags import url
+from django.urls import path, re_path
+
+from locallibrary import settings
 from . import views
+from django.views.static import serve
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,4 +11,6 @@ urlpatterns = [
     path('book_detail/<int:pk>', views.BookDetailView.as_view(), name='book_detail'),
     path('author_list/', views.AuthorListView.as_view(), name='author_list'),
     path('author_detail/<int:pk>', views.AuthorDetailView.as_view(), name='author_detail'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
